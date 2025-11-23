c477feb753345187d6ad53fb6344e358362b95b0
Ricardo Sans <ricardosans98@gmail.com>
2025-11-23 01:57:29 +0100

    add pokemon statuses

---
+++ src/battlefield/status.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+"""
+FYI:
+
+NVS (Non-volatile status): status that remains in the Pokemon even when you take it out from the battle.
+VS (volatile status): temporary status that fades away once you remove the Pokemon from the battlefield.
+"""
+
+class NVStatus(str, Enum):
+    NONE = "not affected by any non-volatile status"
+    BURNT = "burnt"
+    FROZEN = "frozen"
+    PARALIZED = "paralized"
+    POISONED = "poisoned"
+    ASLEEP = "asleep"
+
+
+class VStatus(str, Enum):
+    NONE = "not affected by any non-volatile status"
+    CONFUSED = "confused"
+
+
+def set_nvstatus_from_api(input: str) -> NVStatus:
+    if input == "paralysis":
+        return NVStatus.PARALIZED
+    elif input == "sleep":
+        return NVStatus.ASLEEP
+    elif input == "freeze":
+        return NVStatus.FROZEN
+    elif input == "burn":
+        return NVStatus.BURNT
+    elif input == "poison":
+        return NVStatus.POISONED
+    else:
+        return NVStatus.NONE

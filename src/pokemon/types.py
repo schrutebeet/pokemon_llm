02490c3e0b02a8e1938896225976658369b9580e
Ricardo Sans <ricardosans98@gmail.com>
2025-11-23 01:58:35 +0100

    add pokemon types

---
+++ src/pokemon/types.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class PokemonType(str, Enum):
+    FIRE = "fire"
+    WATER = "water"
+    GRASS = "grass"
+    ELECTRIC = "electric"
+    ICE = "ice"
+    FIGHTING = "fighting"
+    POISON = "poison"
+    GROUND = "ground"
+    FLYING = "flying"
+    PSYCHIC = "psychic"
+    BUG = "bug"
+    ROCK = "rock"
+    GHOST = "ghost"
+    DRAGON = "dragon"
+    DARK = "dark"
+    STEEL = "steel"
+    FAIRY = "fairy"
+    NORMAL = "normal"
